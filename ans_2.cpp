cec0890ca58b46016d745da4c4cf7897eec018f6
Jai Pratap Singh <43641473+jai13598@users.noreply.github.com>
2021-01-04 10:39:22 +0530

    Add files via upload

---
+++ ans_2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        int arr[3];
+        arr[0]=A;
+        arr[1]=B;
+        arr[2]=C;
+        sort(arr,arr+3);
+        return arr[1];
+    }
+};
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  
